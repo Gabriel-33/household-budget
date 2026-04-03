61357a30621d764b6745c9186ea58c983d18ef34
gabriel <gabriel@DESKTOP-NJTVS4S>
2026-04-03 10:20:38 -0300

    refactor: adicionando comentários em arquivo de constants

--- house-holde-web/src/utils/constants.js
+++ house-holde-web/src/utils/constants.js
@@ -1,13 +1,15 @@
 //classe que contém constantes utilizadas no sistema
 export const constants = {
   API_URL: process.env.REACT_APP_API_URL || 'localhost',
-  API_KEY: 'api-key-da-api',
+  API_KEY: 'api-key',
+  //tipo de transação, utilizada em campos de formulários
 
   TIPOS_TRANSACAO: [
     { value: 'despesa', label: 'Despesa' },
     { value: 'receita', label: 'Receita' }
   ],
-  
+
+  //finalidades de categória, utilizada em campos de formulários
   FINALIDADES_CATEGORIA: [
     { value: 'despesa', label: 'Despesa' },
     { value: 'receita', label: 'Receita' },
